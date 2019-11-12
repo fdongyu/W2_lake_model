a25392853ceb6bf2dc1a2ec7cbd8d1d625260bbb
fdongyu <fdongyu2011@gmail.com>
2019-11-12 10:36:51 -0600

    first upload of post-process scripts

---
+++ bathymetry.py
@@ -0,0 +1,245 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 11 09:30:56 2019
+
+@author: dfeng
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import matplotlib.collections as coll
+
+import pdb
+
+class W2_Bathymetry(object):
+    """
+    general class for reading and visualizing CE-QUAL-W2 model bathymetry
+    """
+    Nlyr = 34
+    Nseg = 122
+    
+    def __init__(self, bathfile, **kwargs):
+        self.__dict__.update(kwargs)
+        
+        self.bathfile = bathfile
+        
+    
+    def readVar(self): 
+        
+        self.seg_length = self.readSeg('length')
+        self.seg_ori = self.readSeg('Orientation') 
+        self.lyr_height = self.readSeg('Height')
+        
+        
+        #pdb.set_trace()
+        
+        
+    def readSeg(self, varname='length'):
+        """
+        funtion that reads W2 bathymetry basic info
+        """
+        output_section = []
+        recording = False
+        f = open(self.bathfile, 'r')
+        for s in f:
+            line = s.split()
+            if recording is False:
+                if varname in line:
+                    print "Reading Segment %s ...\n"%varname
+                    recording = True
+            elif recording is True:
+                output_section.append(line)
+                if len(line) == 0:
+                    recording = False
+                    break
+        f.close()
+        
+        return [float(item) for sublist in output_section[:-1] for item in sublist]
+    
+    
+    def readLyr(self):
+        """
+        funtion that reads W2 segment layer info
+        """
+        
+        self.lyrs = np.zeros([self.Nseg, self.Nlyr])
+        self.lyrs_branchID = []
+        self.lyrs_segID = []
+        
+        ## Step 1: read segment ID    
+        f = open(self.bathfile, 'r')
+        for s in f:
+            line = s.split()
+            if 'Branch' in line:
+                #print line
+                #print "Reading Segment %s ...\n"%line 
+                if len(line) == 6:
+                    self.lyrs_branchID.append(line[3])
+                    self.lyrs_segID.append(line[5])
+                elif len(line) == 5:
+                    self.lyrs_branchID.append(line[2])
+                    self.lyrs_segID.append(line[4])
+                elif len(line) == 4:
+                    self.lyrs_branchID.append(line[1])
+                    self.lyrs_segID.append(line[3])
+                
+        f.close()
+        
+        ## Step 2: read Layer from each segment
+        for i in range(len(self.lyrs_segID)):
+            segID = self.lyrs_segID[i]
+            lyr_tem = self.readSegLyr(segID)  
+            #print lyr_tem
+            self.lyrs[i,:len(lyr_tem)] = lyr_tem
+            
+        #pdb.set_trace()
+        
+    
+    def readSegLyr(self, segID):
+        """
+        function that reads the Segment layer given the Seg ID
+        """
+        output_section = []
+        recording = False
+        f = open(self.bathfile, 'r')
+        for s in f:
+            line = s.split()
+            if recording is False:
+                if 'Branch' in line and segID in line[3:]:
+                    print "Reading Segment %s ...\n"%segID
+                    recording = True
+            elif recording is True:
+                output_section.append(line)
+                if len(line) == 0:
+                    recording = False
+                    break
+        f.close()
+        
+        #pdb.set_trace()
+        return [float(item) for sublist in output_section[:-1] for item in sublist]
+    
+    
+    def VisBranch(self, branchID=1):
+        """
+        Given the branch ID, plot the grid at that branch
+        """
+        
+        ## read Segment info
+        self.seg_length = self.readSeg('length')
+        self.lyr_height = self.readSeg('Height')
+        
+        ## read layer info for all segments
+        self.readLyr()
+        
+        lyrs_branchID = np.asarray([int(float(ID)) for ID in self.lyrs_branchID])
+        lyrs_segID = np.asarray([int(float(ID)) for ID in self.lyrs_segID])
+        
+        ## segments in the specified branch
+        segs = lyrs_segID[lyrs_branchID==branchID]
+        seg_length = np.asarray(self.seg_length)[lyrs_branchID==branchID]
+        
+        lyrs = self.lyrs[lyrs_branchID==branchID,:]
+        #pdb.set_trace()
+        ############################################
+        ## The bottom and left rectangle coordinates
+        X = np.cumsum(seg_length) 
+        Z = np.cumsum(np.asarray(self.lyr_height)) 
+        
+        X = X.tolist()
+        Z = Z.tolist()
+        
+        X = [0] + X[:-1]
+        Z = [0] + Z[:-1]
+        ############################################
+        
+        
+        plt.rcParams.update({'font.size': 18})
+        fig = plt.figure(figsize=(11.5,8))
+        ax = fig.add_subplot(111)
+        
+#        pat = []
+        for i in range(len(X)):
+            for j in range(len(Z)):
+#                 ax.axvline(x=X[i], color='k')
+#                 ax.axhline(y=Z[j], color='k')
+                if lyrs[i,j] == 0:  ## masked grid
+                    sq = patches.Rectangle((X[i], Z[j]), seg_length[i], self.lyr_height[j], fill=True)
+                elif lyrs[i,j] != 0:  ## computational grid
+                    sq = patches.Rectangle((X[i], Z[j]), seg_length[i], self.lyr_height[j], fill=False)
+#                pat.append(sq)
+                ax.add_patch(sq)
+
+        #pc = coll.PatchCollection(pat, facecolor=None, edgecolor='k')
+        #ax.add_collection(pc)
+        #ax.relim()
+        ax.title.set_text('Branch %s'%str(branchID))
+        ax.set_xlabel('Distance from upstream (m)')
+        ax.set_ylabel('Water Depth (m)')
+        ax.autoscale_view()
+        plt.gca().invert_yaxis()
+        #plt.show()
+        plt.savefig('figures_grids\grid_branch_%s.png'%str(branchID))
+        plt.close()
+        #pdb.set_trace()
+        
+    def VisBranch2(self, branchID=1):
+        """
+        Given the branch ID, provide the grid to align with velocity output
+        """
+        
+        ## read Segment info
+        self.seg_length = self.readSeg('length')
+        self.lyr_height = self.readSeg('Height')
+        
+        ## read layer info for all segments
+        self.readLyr()
+        
+        lyrs_branchID = np.asarray([int(float(ID)) for ID in self.lyrs_branchID])
+        lyrs_segID = np.asarray([int(float(ID)) for ID in self.lyrs_segID])
+        
+        ## segments in the specified branch
+        segs = lyrs_segID[lyrs_branchID==branchID]
+        seg_length = np.asarray(self.seg_length)[lyrs_branchID==branchID]
+        
+        lyrs = self.lyrs[lyrs_branchID==branchID,:]
+        #pdb.set_trace()
+        ############################################
+        ## The bottom and left rectangle coordinates
+        X = np.cumsum(seg_length) 
+        Z = np.cumsum(np.asarray(self.lyr_height)) 
+        
+        X = X.tolist()
+        Z = Z.tolist()
+        
+        X = [0] + X[:-1]
+        Z = [0] + Z[:-1]
+        
+        #pdb.set_trace()
+        Z = [148.3 - zz for zz in Z]
+        
+        ############################################
+        pat = []
+        for i in range(len(X)):
+            for j in range(len(Z)):
+#                 ax.axvline(x=X[i], color='k')
+#                 ax.axhline(y=Z[j], color='k')
+                if lyrs[i,j] == 0:  ## masked grid
+                    sq = patches.Rectangle((X[i], Z[j]), seg_length[i], self.lyr_height[j], fill=True)
+                elif lyrs[i,j] != 0:  ## computational grid
+                    sq = patches.Rectangle((X[i], Z[j]), seg_length[i], self.lyr_height[j], fill=False)
+                pat.append(sq)
+#                ax.add_patch(sq)
+        
+        return pat
+        
+
+        
+#### For testing ####       
+        
+if __name__ == "__main__":
+    
+    filename = r'C:\Users\dfeng\Downloads\v42\Tests\20191111_baseline3\Bth_WB1.npt'
+    WB = W2_Bathymetry(filename)
+    WB.VisBranch(5)
+    
---
+++ particle_tracking.py
@@ -0,0 +1,293 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  7 15:55:18 2019
+
+@author: dfeng
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
+
+import pdb
+
+
+## file list
+## initialparticle.csv                        the initial state of all particles added or to be added to the system
+## finalparticle.csv                          the final state of all particles released in the model
+## Envrprf_v_particle.csv, envrprf_t_particle.csv, envrprf_depth_particle.csv     histograms of each particle released in the model domain for velocity, temperature and depth.
+
+##  This file has X, Z, Water(=1 if in the water and -1 if above water or in bottom), U(HorizVel), W(VertVel), Temperature, 
+##  DissolvedOxygen, KLayer, and ISegment for each time interval of output.  background to overlay over the particle file
+## Branch1.dat, Branch2.dat, ...
+
+## This file has the location of all the particles as a function of time
+## Part1.dat, Part2.dat, ...    
+
+
+
+class W2_Particle_Tracking(object):
+    """
+    general class for reading and visualizing CE-QUAL-W2 modeled particle tracking
+    """
+    
+    
+    def __init__(self, workdir, **kwargs):
+        self.__dict__.update(kwargs)
+        
+        self.workdir = workdir
+        
+    def ReadBackground(self, branchID='1'):
+        """
+        read Branch1.dat, ... 
+        The background flow
+        """
+        filename = '%s\\Branch%s.dat'%(self.workdir, branchID)
+        
+        ## Step 1: read the timeframe info: JDAY
+        f = open(filename, 'r')
+        JDAYs = []
+        for s in f:
+            line = s.split()
+            if 'ZONE' in line:
+                #print 'Reading Model Run Time JDAY = %s ... \n'%line[2]
+                #pdb.set_trace()
+                JDAYs.append(line[2][:-2])
+        f.close()
+        
+        ## Step 2: read the background flow at each time step
+        self.backflow_sections = []
+        recording = False
+        f = open(filename, 'r')
+        for i in range(len(JDAYs)):
+            ttem = JDAYs[i]
+            new_section = []
+            for s in f:
+                line = s.split()
+                if recording is False:
+                    if ttem+'",' in line:
+                        #pdb.set_trace()
+                        print 'Reading Model Run Time JDAY = %s ... \n'%ttem
+                        recording = True
+                        #pdb.set_trace()
+                        new_section.append(line)
+                elif recording is True:
+                    new_section.append(line)
+                    if len(line)==0:
+                        recording = False
+                        break
+            
+            if i == len(JDAYs)-1:
+                self.backflow_sections.append(new_section[1:])
+            else:
+                self.backflow_sections.append(new_section[1:-1])
+        
+        ## Step 3: get time info     
+        self.runtimes_flow = self.JDAY_con(JDAYs)
+        
+        ##Step 4: get X and Z coordinates    
+        self.X_flow = []
+        self.Z_flow = []
+        self.U = []
+        self.W = []
+        self.T = []
+        for i in range(len(self.backflow_sections)):
+            section = self.backflow_sections[i]
+            xtem = [float(l[0]) for l in section]
+            ztem = [float(p[1]) for p in section]
+            utem = [float(r[3]) for r in section]
+            wtem = [float(q[4]) for q in section]
+            ttem = [float(t[5]) for t in section]
+            self.X_flow.append(xtem)
+            self.Z_flow.append(ztem)
+            self.U.append(utem)
+            self.W.append(wtem)
+            self.T.append(ttem)
+        
+        #pdb.set_trace()
+            
+        
+        
+        
+    def ReadParticles(self, branchID='1'):
+        """
+        read Part1.dat, ...
+        """
+        filename = '%s\\Part%s.dat'%(self.workdir, branchID)
+        
+        
+        ## Step 1: read the timeframe info: JDAY
+        f = open(filename, 'r')
+        JDAYs = []
+        ## Step 1: Read the number of time steps
+        for s in f:
+            line = s.split()
+            if 'ZONE' in line:
+                #print 'Reading Model Run Time JDAY = %s ... \n'%line[2]
+                #pdb.set_trace()
+                JDAYs.append(line[2][:-2])
+        f.close()
+                
+        ## Step 2: read the particle locations at each time step
+        self.output_sections = []
+        recording = False
+        f = open(filename, 'r')
+        for i in range(len(JDAYs)):
+            ttem = JDAYs[i]
+            new_section = []
+            for s in f:
+                line = s.split()
+                if recording is False:
+                    if ttem+'",' in line:
+                        print 'Reading Model Run Time JDAY = %s ... \n'%ttem
+                        recording = True
+                        #pdb.set_trace()
+                        new_section.append(line)
+                elif recording is True:
+                    new_section.append(line)
+                    if 'TEXT' in line:
+                        recording = False
+                        break
+            
+            self.output_sections.append(new_section[1:-1])
+            #pdb.set_trace()
+            
+        
+        if len(JDAYs) != len(self.output_sections):
+            raise Exception('Read data not successful, double check!!\n')
+            
+            
+        ## Step 3: get time info     
+        self.runtimes = self.JDAY_con(JDAYs)
+        
+        ## Step 4: get X and Z coordinates    
+        self.X = []
+        self.Z = []
+        for i in range(len(self.output_sections)):
+            section = self.output_sections[i]
+            xtem = [float(l[0]) for l in section]
+            ztem = [float(p[1]) for p in section]
+            self.X.append(xtem)
+            self.Z.append(ztem)
+            #pdb.set_trace()
+            
+            
+            
+        #pdb.set_trace()
+        
+    def VisParticles(self, timestep=-1, PlotFlow=True, PlotTemp=True, PlotGrid=True):
+        """
+        visualize the particle trajectories at a given time step
+        set PlotTemp == False for now, need to figure out how to contour plot 1D array
+        """
+        self.ReadParticles('1')
+        
+        X = self.X[timestep]
+        Z = self.Z[timestep]
+        
+        if PlotFlow == True or PlotTemp == True:
+            self.ReadBackground('1')
+            X_flow = self.X_flow[timestep]
+            Z_flow = self.Z_flow[timestep]
+        
+        plt.rcParams.update({'font.size': 18})
+        #fig = plt.figure(figsize=(11.5,10))
+        fig = plt.figure(figsize=(11.5,8))
+        ax = fig.add_subplot(111)
+        ax.plot(X, Z, '.', color = 'r')
+        if PlotFlow == True:
+            U = self.U[timestep]
+            W = self.W[timestep]
+            scale = 1.
+            scale = 100./scale
+            Q = ax.quiver(X_flow, Z_flow, np.asarray(U)*100., np.asarray(W)*100.,
+                               zorder=5, width=0.001, headwidth=4, headlength=4.5,
+                              scale=scale)
+            qk = ax.quiverkey(Q, 0.15, 0.15, 1, r'$1 \frac{cm}{s}$', labelpos='W',fontproperties={'weight': 'bold','size':20})
+        
+        if PlotTemp == True:
+            #from scipy.interpolate import griddata
+            T = self.T[timestep]
+            #xgrid, zgrid = np.meshgrid(X_flow, Z_flow)
+            #Tgrid = griddata((X_flow, Z_flow), T, (xgrid, zgrid))
+            #ax.contourf(xgrid, zgrid, Tgrid, 10, cmap=plt.cm.bone)
+            cs = ax.tricontourf(X_flow, Z_flow, T, cmap=plt.cm.bone)
+            
+            from mpl_toolkits.axes_grid1 import make_axes_locatable
+            divider = make_axes_locatable(ax)
+            cax = divider.append_axes("right", size="5%", pad=0.05)
+            cb = fig.colorbar(cs, cax=cax, orientation='vertical')
+            cb.ax.tick_params(labelsize=18)
+            cb.ax.yaxis.offsetText.set_fontsize(14)
+            cb.set_label('Temperature', fontsize=18)
+            
+        if PlotGrid == True:
+            from bathymetry import W2_Bathymetry
+            filename = '%s\\%s'%(self.workdir, 'Bth_WB1.npt')
+            WB = W2_Bathymetry(filename)
+            pat = WB.VisBranch2(1)
+            for sq in pat:
+                ax.add_patch(sq)
+            ax.autoscale_view()
+            
+        timestr = datetime.strftime(self.runtimes[timestep],'%Y-%m-%d')
+        ax.title.set_text('Time: %s'%timestr)
+        #ax.set_xlim([4000, 12000])
+        ax.set_ylim([125, 155])
+        ax.set_xlabel('Distance from upstream (m)')
+        ax.set_ylabel('Water Depth (m)')
+        ax.yaxis.grid(True)
+        ax.xaxis.grid(True)
+        plt.savefig('simple_particle_tracks_%s.png'%str(timestep))
+        #plt.savefig('example.png')
+        plt.close()
+        #plt.show()
+    
+        
+    def VisParticles_full(self):
+        """
+        visualize the particle trajectories, the particles at all time steps are plotted
+        """
+        self.ReadParticles('1')
+    
+        X = [item for sublist in self.X for item in sublist]
+        Z = [item for sublist in self.Z for item in sublist]
+        
+        plt.rcParams.update({'font.size': 18})
+        fig = plt.figure(figsize=(11.5,6))
+        ax = fig.add_subplot(111)
+        ax.plot(X, Z, '.', color = 'k')
+        ax.set_xlim([4000, 12000])
+        ax.set_ylim([140, 150])
+        ax.yaxis.grid(True)
+        ax.xaxis.grid(True)
+        plt.savefig('simple_particle_tracks.png')
+        plt.close()
+        #plt.show()
+        
+        #pdb.set_trace()
+        
+        
+        
+    def JDAY_con(self,JDAY):
+        """
+        funtion adapted from Justin's code
+        """
+        basedate = datetime(2010,12,31)
+        return_date = [basedate+timedelta(days = float(ii)) for ii in JDAY]
+        return return_date
+        
+        
+        
+#### For testing ####       
+        
+if __name__ == "__main__":
+    
+    #wdir = r'C:\Users\dfeng\Downloads\v42\Examples\Particle tracking_DF'
+    #wdir = r'C:\Users\dfeng\Downloads\v42\Tests\20191106_baseline'
+    #wdir = r'C:\Users\dfeng\Downloads\v42\Tests\20191108_baseline2'
+    wdir = r'C:\Users\dfeng\Downloads\v42\Tests\20191111_baseline3'
+    WPT = W2_Particle_Tracking(wdir)
+    WPT.VisParticles(310)
+    
+    
